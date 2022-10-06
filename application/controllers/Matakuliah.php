973e74c0a56ce13fad633efa7f16cb1666ea8585
balonkuadadua <iqtaxation@gmail.com>
2022-10-06 21:00:54 +0700

    latihan-5

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -8,11 +8,32 @@ class Matakuliah extends CI_Controller
 
     public function cetak()
     {
-        $data =[
-            'kode' => $this->input->post('kode'),
-            'nama' => $this->input->post('nama'),
-            'sks' => $this->input->post('sks')
-        ];
-        $this->load->view('view-data-matakuliah', $data);
+        $this->load->library('form_validation');
+//        $data =[
+//            'kode' => $this->input->post('kode'),
+//            'nama' => $this->input->post('nama'),
+//            'sks' => $this->input->post('sks')
+//        ];
+//        $this->load->view('view-data-matakuliah', $data);
+        $this->form_validation->set_rules('kode','Kode Matakuliah','required|min_length[3]',[
+            'required'=>'kode Matakuliah Harus diisi',
+            'min_length'=>'Kode terlalu pendek'
+        ]);
+
+        $this->form_validation->set_rules('nama','Nama Matakuliah','required|min_length[3]',[
+            'required'=>'nama Matakuliah Harus diisi',
+            'min_length'=>'nama terlalu pendek'
+        ]);
+
+        if($this->form_validation->run() !=true){
+            $this->load->view('view-form-matakuliah');  
+        } else {
+            $data =[
+                'kode'=>$this->input->post('kode'),
+                'nama'=>$this->input->post('nama'),
+                'sks'=>$this->input->post('sks')
+            ];
+            $this->load->view('view-data-matakuliah',$data);
+        }
     }
 }
---
+++ application/views/v_footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <a href="https://www.rentalbuku.com">RentalBuku</a>
+</footer>
+</div>
+</body>
+</html>
